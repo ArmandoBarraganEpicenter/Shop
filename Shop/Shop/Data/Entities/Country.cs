51ebb40997e8838462b1c5dd53c5de6154f75349
Andrea Davila <armando.barragan@epicenter.com.co>
2023-04-22 11:28:46 +0800

    Finisdh countries CRUD

---
+++ Shop/Shop/Data/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage="El campo {0} es obligatorio.")]
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener máximo {1} carácteres.")]
+        public string Name { get; set; }
+
+    }
+}
